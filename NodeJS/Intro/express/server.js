85b6ac2a2a61a83ebe2e18554913711f93d738ff
Abhishek <abhishekgoel@gofynd.com>
2025-04-17 23:19:50 +0530

    midl=dlewares

--- NodeJS/Intro/express/server.js
+++ NodeJS/Intro/express/server.js
@@ -4,6 +4,43 @@ const app = express();
 //it parses incoming JSON and makes it available in req.body
 // req.body = JSON.parse(data)
 app.use(express.json());
+//serves the static html css or js files from public folder 
+app.use(express.static("public"));
+
+const middleware1 = async(req, res ,next) => {
+    console.log(req.method);
+    console.log(req.url);
+    console.log("This is first checkpoint");
+    next(); 
+}
+
+const middleware2 = async(req, res ,next) => {
+    console.log("This is 2nd checkpoint");
+    next(); 
+}
+//general / global middleware
+// log/transform data 
+//validate the data
+//perform auth checks 
+app.use(middleware1);
+app.use(middleware2);
+
+const performSanity = async(req, res, next) => {
+    if(!req.body.email || !req.body.password){
+        console.log("error");
+        return res.send("bad request");
+    }
+    console.log("performing basic sanity");
+    console.log("checked the data");
+    next();
+}
+
+const checkAuth = async(req, res, next) => {
+    console.log("checking for valid email");
+    console.log("checked the data");
+    next();
+}
+
 
 app.get("/", (req, res) => {
     res.send("Hello World !")
@@ -13,7 +50,7 @@ app.get("/login", (req, res) => {
     res.send("Please Login !")
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", performSanity, checkAuth,(req, res) => {
     console.log(req.body);
     res.send("Login successfull!")
 });
